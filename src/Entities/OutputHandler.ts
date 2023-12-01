60e255bd26a5bf3270e1bd8d3f7bd2ebfdd668ce
Lucas Ramalho <businessramalho@gmail.com>
2023-12-01 11:28:23 -0300

    feature, método para mostrar saida dos dados do livro

--- src/Entities/OutputHandler.ts
+++ src/Entities/OutputHandler.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
+import { Livro } from './Livro'
 
 export class OutputHandler {
   static SuccessOutput(text: string) {
@@ -11,5 +12,21 @@ export class OutputHandler {
 
   static NormalOutput(text: string) {
     console.log(text)
+  }
+
+  static mostrarLivroInfo(livro: Livro) {
+    const exemplares = livro.obterExemplares()
+    console.log('----------------------------')
+    console.log(`Titulo: ${chalk.green(livro.getTitulo())}`)
+    console.log('Quantidade reservas: ')
+    console.log('----------------------------')
+    console.log('Examplares: ')
+    exemplares.forEach(exemplar => {
+      console.log(`\t - Código exemplar: ${exemplar.getCodigo()}`)
+      const status = exemplar.getStatus()
+      console.log(`\t - Status: ${status == 'Disponível' ? chalk.green(status) : chalk.red(status)}`)
+      console.log('++++++++++++++++++++++++++++')
+    })
+    console.log('----------------------------')
   }
 }

91dc58ee889b46ff0b1bb1ab7ed5d81aab83f42f
Lucas Ramalho <businessramalho@gmail.com>
2023-12-05 16:58:32 -0300

    fix, método de notificacao de usuarios

--- src/Entities/Publisher.ts
+++ src/Entities/Publisher.ts
@@ -7,6 +7,7 @@ export class Publisher {
   private subscribers: Map<string, ISubscribers> = new Map()
 
   public subscribe(codigoLivro: number, usuario: Professor): void {
+    console.log(`Adicionado: ${usuario.obterNome()}`)
     const subscriber: ISubscribers = new Subscriber(usuario); // Lidar com os efeitos colaterais de ser professor
     const key = `${codigoLivro}__${usuario.obterCodigo()}`
     this.subscribers.set(key, subscriber)
@@ -20,8 +21,8 @@ export class Publisher {
   public notify(codigoLivro: number): void {
     const entradas = this.subscribers.entries()
     for(let sub of entradas) {
-      const [codigoLivro, codigoUsuario] = sub[0].split('__')
-      if(sub[0].includes(codigoLivro.toString())) {
+      const [codigoLivroNotficado, codigoUsuario] = sub[0].split('__')
+      if(codigoLivro == Number.parseInt(codigoLivroNotficado)) {
         sub[1].update(Number.parseInt(codigoUsuario))
       }
     }
